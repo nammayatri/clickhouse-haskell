a85faf5d2ba9e3093d2edd2beb182259524855d7
Sridhar Ratnakumar <srid@srid.ca>
2023-05-18 16:54:11 -0400

    Run ormolu (via pre-commit run -a)

--- src/Database/ClickHouseDriver.hs
+++ src/Database/ClickHouseDriver.hs
@@ -1,8 +1,8 @@
-
-module Database.ClickHouseDriver( 
-  module Database.ClickHouseDriver.Defines,
-  module Database.ClickHouseDriver.HTTP,
-) where
+module Database.ClickHouseDriver
+  ( module Database.ClickHouseDriver.Defines,
+    module Database.ClickHouseDriver.HTTP,
+  )
+where
 
 import Database.ClickHouseDriver.Defines
 import Database.ClickHouseDriver.HTTP
